263607109c46d7f8c0b91d526a2e9b35eac18b7f
Jose <josealejandrogilmendez08@gmail.com>
2022-11-27 10:14:01 -0400

    set up files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const homepage =(() =>{
+    console.log('homepage');
+})();
+
+const todoItem = (title, description, dueDate, priority) =>{
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
